8ecd35ddb36de423166518b0052a07b692bfd790
vbedekovic16 <vb52325@fer.hr>
2023-01-04 16:39:55 +0100

    Added schema.org context

--- backend/services/responseWrapper.ts
+++ backend/services/responseWrapper.ts
@@ -1,5 +1,31 @@
+const contextObj = 
+    {
+        "@context": {
+            "@vocab": "http://schema.org/",
+            "workout_type": "exerciseType",
+            "weekday": "dayOfWeek"
+        }
+    }
+
+
 
 export function responseWrap(status, message, response = null) {
+    if (Array.isArray(response)) {
+        for (let i = 0; i < response.length; i++) {
+            response[i] = 
+                {
+                    ...contextObj,
+                    ...response[i]
+                }
+        }
+    } else if (response) {
+        response = {
+            ...contextObj,
+            ...response
+        }
+    }
+
+
     let responseObj = {
         status: status,
         message: message,
